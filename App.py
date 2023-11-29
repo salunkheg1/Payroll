ddd31b9b086e44819772ae919c02259ec673ae2f
Ganesh <salunkheg1@gmail.com>
2023-11-29 11:27:37 +0530

    install flash and open html page using flash

--- App.py
+++ App.py
@@ -1,2 +1,16 @@
-print("hellow")
-print("How are you?")
+from flask import Flask,render_template
+app = Flask(__name__)
+@app.route("/")
+def hello_world():
+    return render_template('index.html')
+
+@app.route("/Ganesh")
+def Ganesh():
+    return "<p>Welcome Ganesh___</p>"
+
+@app.route("/about")
+def about():
+    name = "Ganesh"
+    return render_template("about.html",name1=name)
+
+app.run(debug=True)
