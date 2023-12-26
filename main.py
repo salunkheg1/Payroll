c9074a167b1cddc0764b62cc5a6ae715d405fa9c
Ganesh <salunkheg1@gmail.com>
2023-12-26 15:26:55 +0530

    Add Entry in DB

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+from flask_sqlalchemy import SQLAlchemy
+
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/payroll'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    USER_ID = db.Column(db.Integer, primary_key=True)
+    USER_NAME = db.Column(db.String(80), nullable=False)
+    PASSWORD = db.Column(db.String(12), nullable=False)
+    ROLL = db.Column(db.String(120), nullable=False)
+
+
 @app.route("/")
 def home():
     return render_template('index.html')
@@ -21,9 +34,19 @@ def post():
     return render_template('post.html')
 
 
-@app.route("/LOGIN.html")
+
+@app.route("/login.html", methods = ['GET', 'POST'])
 def login():
-    return render_template('LOGIN.html')
+    roll="ADMIN"
+    if(request.method=='POST'):
+        name = request.form.get('username')
+        password = request.form.get('password')
+        entry = User(USER_NAME = name, PASSWORD = password , ROLL=roll)
+        db.session.add(entry)
+        db.session.commit()
+    return render_template('login.html')
+
+
 
 @app.route("/Ganesh")
 def Ganesh():
